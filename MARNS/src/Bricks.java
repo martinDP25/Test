20741280bcc412bc134ad877da0a9723896de2dd
martinDP25 <matthew.dasilva.94@gmail.com>
2018-09-22 23:37:47 -0400

    First

---
+++ MARNS/src/Bricks.java
@@ -0,0 +1,7 @@
+
+public class Bricks {
+
+	public void takeMyBrick() {
+		System.out.println("It's a heavy one!!");
+	}
+}
---
+++ MARNS/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		Bricks bricksObject = new Bricks();
+		bricksObject.takeMyBrick();
+		
+
+
+	}
+
+}
